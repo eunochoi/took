20393add12b828e9f8df7a3f60571b35b7a5823c
euno <chonist@gmail.com>
2026-06-20 22:30:44 +0900

    chore: remove legacy express backend

--- back/function/decrypt.js
+++
@@ -1,9 +0,0 @@
-// 외부 패키지
-const CryptoJS = require('crypto-js');
-
-//암호화 처리
-const decrypt = (content, secretKey) => {
-  return CryptoJS.AES.decrypt(content, secretKey).toString(CryptoJS.enc.Utf8);
-};
-
-module.exports = decrypt;
--- back/function/encrypt.js
+++
@@ -1,9 +0,0 @@
-// 외부 패키지
-const CryptoJS = require('crypto-js');
-
-//암호화 처리
-const encrypt = (content, secretKey) => {
-  return CryptoJS.AES.encrypt(content, secretKey).toString();
-};
-
-module.exports = encrypt;
