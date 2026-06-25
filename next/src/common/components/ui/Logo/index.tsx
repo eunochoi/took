544eeb60137cb805982e488f5210aa04505d0218
euno <chonist@gmail.com>
2026-06-25 12:49:12 +0900

    fix: add withText prop to Logo component for conditional text rendering

--- next/src/common/components/ui/Logo/index.tsx
+++ next/src/common/components/ui/Logo/index.tsx
@@ -7,9 +7,10 @@ import emotionsImage from '/public/img/emotion/emotions.png';
 interface LogoProps {
   size?: number;
   className?: string;
+  withText?: boolean;
 }
 
-const Logo = ({ size = 36, className }: LogoProps) => {
+const Logo = ({ size = 36, withText = true, className }: LogoProps) => {
   const imageSize = size * 5;
 
   return (
@@ -17,12 +18,12 @@ const Logo = ({ size = 36, className }: LogoProps) => {
       <div className="flex items-center justify-center">
         <Image src={emotionsImage} alt="TOOK Logo" width={imageSize} height={imageSize} priority />
       </div>
-      <span
+      {withText && <span
         className="uppercase leading-[1.2] text-[#5C5C5C]"
         style={{ fontFamily: "var(--font-joti-one), 'Joti One', sans-serif", fontSize: `${size}px` }}
       >
         TOOK
-      </span>
+      </span>}
     </div>
   );
 };
