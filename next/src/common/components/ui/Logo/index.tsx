fd184ff21cfb0c1989b5a71e126869c5bf2d6e37
euno <chonist@gmail.com>
2026-07-28 17:56:07 +0900

    refactor: change default value of withText prop in Logo component to false

--- next/src/common/components/ui/Logo/index.tsx
+++ next/src/common/components/ui/Logo/index.tsx
@@ -10,7 +10,7 @@ interface LogoProps {
   withText?: boolean;
 }
 
-const Logo = ({ size = 36, withText = true, className }: LogoProps) => {
+const Logo = ({ size = 36, withText = false, className }: LogoProps) => {
   const imageSize = size * 5;
 
   return (
