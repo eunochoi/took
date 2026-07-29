75bd839974722042bb2c58c32ff267ef0aa201c0
euno <chonist@gmail.com>
2026-07-29 18:42:46 +0900

    feat: update font size in GreetingSection and globals.css for improved readability

--- next/src/app/(routes)/(app)/home/_components/GreetingSection.tsx
+++ next/src/app/(routes)/(app)/home/_components/GreetingSection.tsx
@@ -25,7 +25,7 @@ const GreetingSection = () => {
         />
       </div>
       <span className="text-3xl font-title font-bold capitalize text-theme-accent">툭! 오늘도 하나씩 :)</span>
-      <p className="m-0 flex flex-col gap-1 break-words text-justify text-base leading-normal text-theme-text-primary">
+      <p className="m-0 flex flex-col gap-1 break-words text-justify text-lg leading-normal text-theme-text-primary">
         <span className="block">완벽한 하루가 아니어도 좋습니다.</span>
         <span className="block">습관 발자국 하나만 남겨도 충분해요.</span>
       </p>
