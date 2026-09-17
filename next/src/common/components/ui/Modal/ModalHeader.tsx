252f240d1e4cb18de08801a1ae275abb8585a96c
euno <chonist@gmail.com>
2026-09-17 17:33:14 +0900

    style: update font class in ModalHeader component

--- next/src/common/components/ui/Modal/ModalHeader.tsx
+++ next/src/common/components/ui/Modal/ModalHeader.tsx
@@ -14,7 +14,7 @@ interface ModalHeaderProps {
 
 const modalHeaderClass = "relative flex h-[var(--mobileHeader)] w-full shrink-0 items-center justify-between px-[4dvw] tablet:px-6";
 const modalHeaderButtonClass = "flex items-center justify-center text-theme-accent";
-const modalHeaderTitleClass = "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-base text-theme-text-primary";
+const modalHeaderTitleClass = "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-title text-base text-theme-text-primary";
 
 export const ModalHeader = ({
   className,
