1b665edb350280e9a43f2c9b55b5dc90fd53a35c
euno <chonist@gmail.com>
2026-09-24 15:22:02 +0900

    fix: expand scroll boundary detection area

--- next/src/common/components/ui/ScrollContainer/index.tsx
+++ next/src/common/components/ui/ScrollContainer/index.tsx
@@ -51,13 +51,18 @@ export const ScrollContainer = forwardRef<HTMLDivElement, ScrollContainerProps>(
     };
 
     return (
+      // 상하단 fade, top button을 주기위한 wrapper, 전체 틀
       <div className={cn("relative min-h-0 w-full overflow-hidden", className)}>
+        {/* 내부 컨텐츠가 증가함에 따라 스크롤이 생기는 컨테이너 */}
         <div
           ref={setScrollArea}
           className={cn("h-full w-full overflow-y-auto", scrollAreaClassName)}
           {...props}
         >
+          {/* 내부 컨텐츠가 늘어남에 따라 같이 높어나며 컨텐츠를 묶어주는 컨테이너 */}
+          {/* 컨텐츠가 높이 변화에 따라 top, bottom에 스크롤 감지를 위한 바운더리가 위치 */}
           <div className={cn("relative min-h-full w-full shrink-0", contentClassName)}>
+            {/* 스크롤 감지를 위한 바운더리, absoulte로 위치가 지정되어 높이가 컨텐츠 감싸는 컨테이너에 영향을 끼치지 못함 */}
             <ScrollBoundary ref={topBoundaryRef} edge="top" />
             {children}
             <ScrollBoundary ref={bottomBoundaryRef} edge="bottom" />
