bb850880cb18ddf38bda2f50a796973994458a23
euno <chonist@gmail.com>
2026-09-25 04:57:33 +0900

    feat: add illustrated habit page header

--- next/src/app/(routes)/(app)/habit/HabitView.client.tsx
+++ next/src/app/(routes)/(app)/habit/HabitView.client.tsx
@@ -11,11 +11,13 @@ import { MAX_HABIT_COUNT } from "@/common/constants/habit";
 import { usePrefetchPage } from "@/common/hooks/usePrefetchPage";
 import { useSortToggle } from "@/common/hooks/useSortToggle";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { enqueueSnackbar } from "notistack";
 import { useEffect, useRef } from "react";
 import { useCustomHabitOrder } from "./_hooks/useCustomHabitOrder";
 import { useTodayHabitRate } from "./_hooks/useTodayHabitRate";
+import bottomCat from '/public/img/bottom-cat.png';
 
 const HabitView = () => {
   usePrefetchPage();
@@ -47,8 +49,13 @@ const HabitView = () => {
 
   return (
     <AppPageLayout
-      contentWrapperClassName={APP_PAGE_CONTENT_PADDING_CLASS_NAME}
-      appPageTopArea={<AppPageTitle title="습관 만들기" description="작은 실천으로 만들어가는 나의 일상" />}
+      appPageTopArea={
+        <div className={`bg-theme-accent-light ${APP_PAGE_CONTENT_PADDING_CLASS_NAME}`}>
+          <AppPageTitle title="습관 만들기" description="작은 실천으로 만들어가는 나의 일상" />
+          <Image src={bottomCat} alt="bottom-cat" className="ml-auto mt-auto block w-full tablet:w-2/3 desktop:w-2/3" />
+        </div>
+      }
+      mainAreaClassName="flex-1 bg-theme-surface px-[4dvw] py-2 tablet:px-9 tablet:py-4 desktop:px-14 desktop:py-8"
       pageRef={pageRef}
       showScrollToTop
       toolbar={
